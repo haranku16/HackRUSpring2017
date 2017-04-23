4e260c34cce6eb09c9978b89d5553b70b59629c4
Harankumar Nallasivan <haranku16@gmail.com>
2017-04-22 23:59:43 -0400

    My commit

--- audio/recorder.py
+++ audio/recorder.py
@@ -1,37 +1,99 @@
+import threshold_learner
 import pyaudio
-import wave
-import audioop
 import numpy
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+import httplib
+import threading
+import time
+import json
+from synchronized import synchronized
 
-p = pyaudio.PyAudio()
+class Threshold:
+	def __init__(self):
+		self.threshold = 0.0
+	@synchronized
+	def learn(self):
+		self.threshold = threshold_learner.learn()
+	@synchronized
+	def get(self):
+		return self.threshold
 
-stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer=CHUNK)
+class RunStatus:
+	def __init__(self):
+		self.status = False
+	@synchronized
+	def flip(self):
+		self.status = not self.status
+	@synchronized
+	def get(self):
+		return self.status
 
-print("* recording")
+threshold = Threshold()
 
-frames = [] 
-ampSum = 0
+running = RunStatus()
 
-for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
-	data = stream.read(CHUNK)
-	ampSum += numpy.sum(numpy.square(numpy.fromstring(data, numpy.int16).astype(numpy.int64))) 
-print("* recording")
+ser = '0c812423eca0798887dc28479baa08a8818adb183a7792c4e07325a47469877f4bd4e76023e66413c3d6cd6417e05170'
+privkey = 'd93d01e5dbb470f605481898b66cc97f6d3f793cffe5d12ccb83944fd0b509b0fded28ae99e58bf6654af03b36fc497b'
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+def makeJson():
+	m = { 'serialNumber':ser,'privateKey':privkey }
+	return json.dumps(m,True)
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+def sSend():
+	conn = httplib.HTTPConnection('adapter.cs.rutgers.edu:3000')
+	print 'Sending:',makeJson()
+	conn.request('POST','/device-event',makeJson())
+	res = conn.getresponse()
+	print(res.status)
+	print('Sent message to server, got response status:',res)
+	
+def sListen():
+	CHUNK = 440000
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+        RECORD_SECONDS = 5
+        WAVE_OUTPUT_FILENAME = "output.wav"
 
+        p = pyaudio.PyAudio()
 
+        stream = p.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True, frames_per_buffer=CHUNK)
+	
+        print("* recording learning sample")
+
+	while running.get():
+		data = stream.read(CHUNK)
+		frames = numpy.fromstring(data, numpy.int16).astype(numpy.int64)
+		curramp = numpy.average(numpy.square(frames))
+		print 'Current amp is ',curramp
+		if(curramp > threshold.get()):
+			sSend()
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+if __name__ == "__main__":
+	thread1 = False
+	while True:
+		case = raw_input('Enter learn, start, stop or quit\n')
+		if(case == 'learn'):
+			threshold.learn()
+			print threshold.get()
+		if(case == 'start'):
+			if not running.get():
+				running.flip()
+				thread1 = threading.Thread(group = None, target=sListen)	
+				thread1.start()		
+			else:
+				print('Recorder already running.')
+		if(case == 'stop'):
+			if running.get():
+				running.flip()
+				thread1.join()
+			else:
+				print('Recorder is not running.')
+			
+		if(case == 'quit'):
+			if running.get():
+				running.flip()
+			if thread1 != False: 
+				thread1.join()
+			break
